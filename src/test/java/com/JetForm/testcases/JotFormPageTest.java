ba756224ef29c6e5ec4b0d820c56c50e229945d8
kmaheshw <kanupriyam@ymail.com>
2021-03-07 01:48:18 -0500

    Update JotFormPageTest.java

--- src/test/java/com/JetForm/testcases/JotFormPageTest.java
+++ src/test/java/com/JetForm/testcases/JotFormPageTest.java
@@ -28,27 +28,30 @@ public class JotFormPageTest extends TestBase {
 		// testUtil = new TestUtil();
 	}
 	
-	@Test(priority=0)
+	@Test (priority = 0)
+	//@Test()
 	public void verifyHomePageTitleTest(){ //Method to verify home page title
 		String homePageTitle = jotFormPage.verifyPageTitle();
 	String message="Actual title is " + homePageTitle;
 		Assert.assertEquals(homePageTitle, "Hotel Booking Form", message);
 	}
 	
-	/*@Parameters({ "fName", "lName" ,"emailId", "roomTypeEntry", "NumberOfGuest","ArrivalDate","departureMonth","departureDay", "departureYear","flight","specialRequest"})
-	@Test(priority=1)
+	@Parameters({ "fName", "lName" ,"emailId", "roomTypeEntry", "NumberOfGuest","ArrivalDate","departureMonth","departureDay", "departureYear","flight","specialRequest"})
+	@Test (priority = 1)
+	//@Test()
 	public void EnterFormInformation(String fName,String lName,String emailId, String roomTypeEntry, String NoOfGuest,String ArrivalDate,String departureMonth,String departureDay, String departureYear,String flight,String specialRequest) throws AWTException{ //Method to verify home page title
 		jotFormPage.fillForm(fName,lName,emailId,roomTypeEntry,NoOfGuest,ArrivalDate,departureMonth,departureDay,departureYear,flight,specialRequest);
 		
 		
 	}*/
+	//@Test()
+	@Test (priority = 2)
 	
-	/*@Test(priority=2)
-	public void verifyTitleTest(){ //Method to verify home page title
+		public void verifyTitleTest(){ //Method to verify home page title
 		String homePageTitle = jotFormPage.verifyPageTitle();
 		String message="Actual title is " + homePageTitle;
 		Assert.assertEquals(homePageTitle, "Thank You", message);
-	}*/
+	}
 	
 	
 	@AfterClass
