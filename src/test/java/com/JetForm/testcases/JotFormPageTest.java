2f688cfb75a5c1823e7dc25e434eb1c34cc5666c
kmaheshw <kanupriyam@ymail.com>
2021-03-07 01:58:43 -0500

    Update JotFormPageTest.java

--- src/test/java/com/JetForm/testcases/JotFormPageTest.java
+++ src/test/java/com/JetForm/testcases/JotFormPageTest.java
@@ -28,23 +28,26 @@ public class JotFormPageTest extends TestBase {
 		// testUtil = new TestUtil();
 	}
 	
-	@Test (priority = 0)
-	public void verifyHomePageTitleTest(){ //Method to verify home page title
+	//@Test (priority = 0)
+	@Test()
+	public void AAAverifyHomePageTitleTest(){ //Method to verify home page title
 		String homePageTitle = jotFormPage.verifyPageTitle();
 	String message="Actual title is " + homePageTitle;
 		Assert.assertEquals(homePageTitle, "Hotel Booking Form", message);
 	}
 	
 	@Parameters({ "fName", "lName" ,"emailId", "roomTypeEntry", "NumberOfGuest","ArrivalDate","departureMonth","departureDay", "departureYear","flight","specialRequest"})
-	@Test (priority = 1)
-	public void EnterFormInformation(String fName,String lName,String emailId, String roomTypeEntry, String NoOfGuest,String ArrivalDate,String departureMonth,String departureDay, String departureYear,String flight,String specialRequest) throws AWTException{ //Method to verify home page title
+	//@Test (priority = 1)
+	@Test()
+	public void BBBEnterFormInformation(String fName,String lName,String emailId, String roomTypeEntry, String NoOfGuest,String ArrivalDate,String departureMonth,String departureDay, String departureYear,String flight,String specialRequest) throws AWTException{ //Method to verify home page title
 		jotFormPage.fillForm(fName,lName,emailId,roomTypeEntry,NoOfGuest,ArrivalDate,departureMonth,departureDay,departureYear,flight,specialRequest);
 		
 		
 	}
 	
-	@Test (priority = 2)
-		public void verifyTitleTest(){ //Method to verify home page title
+	//@Test (priority = 2)
+	  @Test()
+		public void CCCverifyTitleTest(){ //Method to verify home page title
 		String homePageTitle = jotFormPage.verifyPageTitle();
 		String message="Actual title is " + homePageTitle;
 		Assert.assertEquals(homePageTitle, "Thank You", message);
